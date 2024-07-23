a94c22b152ee376b0aea30e890f1f7c2fb843a9f
Daniel <danielyoussef1@yahoo.com>
2024-07-23 07:23:38 -0700

    completed operation functions

---
+++ script.js
@@ -0,0 +1,36 @@
+const num1 = 0;
+const num2 = 0;
+const operator = "";
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  if (operator == " +") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2);
+  } else if (operator == "*") {
+    return multiply(num1, num2);
+  } else if (operator == "/") {
+    return divide(num1, num2);
+  }
+}
+
+console.log("Add: " + add(num1, num2));
+console.log("Subtract: " + subtract(num1, num2));
+console.log("Multiply: " + multiply(num1, num2));
+console.log("Divide: " + divide(num1, num2));
